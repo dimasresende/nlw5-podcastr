6bc55ded0f9bf5682a3c16e50185685da44c6076
Dimas Resende <dresende@uolinc.com>
2021-04-21 02:25:41 -0300

    Consumindo API com abordagem SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+// Faz o fetch dos episódios do lado do servidor usando a abordagem de SSG para fazer cache do retorno
+// No retorno a chave 'revalidate' é o tempo que ele vai cachear até fazer o request novamente
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
