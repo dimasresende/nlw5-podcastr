0ce9d0639b1627077106157a2b2989f81cfa8b69
Dimas Resende <dresende@uolinc.com>
2021-04-21 20:51:21 -0300

    Fazendo a tipagem dos dados da API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,18 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+
+//Tipagem de cada episódio
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+//Tipagem das propriedades (que vai ser um array de episódios)
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -9,7 +23,7 @@ export default function Home(props) {
 
 // Faz o fetch dos episódios do lado do servidor usando a abordagem de SSG para fazer cache do retorno
 // No retorno a chave 'revalidate' é o tempo que ele vai cachear até fazer o request novamente
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3333/episodes');
   const data = await response.json();
 
